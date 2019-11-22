d880b8b9d093258bef60182f8069d8a20a57d9ab
Will Hathaway <william.hathaway38@gmail.com>
2019-11-21 20:21:19 -0500

    added console logs, comments, and inquirer prompt

--- liri.js
+++ liri.js
@@ -4,49 +4,83 @@ exports.printMsg = function () {
 
 require("dotenv").config();
 
-let fs = require("fs");
+// external file requirements assigned to universal variables:
+
+const fs = require("fs");
+
+const inquirer = require("inquirer");
+
+// the keys.js file contains an object with the necessary spotify keys:
 
 const keys = require("./keys.js");
 
+// axios module allows for api calls to be made:
+
 const axios = require("axios")
+
+// the spotify node api module allows api calls to be made to spotify:
 
 const Spotify = require("node-spotify-api");
 
 let spotify = new Spotify(keys.spotify);
 
-let command = process.argv[2];
 
-let input = process.argv.splice(3, process.argv.length).join();
+inquirer.prompt([{
+        type: "list",
+        name: "command",
+        choices: ["song", "movie", "concert"]
+    },
+    {
+        type: "input",
+        name: "input",
+        message: "Enter song/movie/artist",
+    }
+]).then(function(user) {
 
-switch (command) {
-    case "movie":
-        movie(input);
-        break;
-    case "song":
-        song(input);
-        break;
-    case "concert":
-        concert(input);
-        break;
-}
+    // switch statement checks the command and calls the corresponding function:
 
+    switch (user.command) {
+        case "movie":
+            movie(user.input);
+            break;
+        case "song":
+            song(user.input);
+            break;
+        case "concert":
+            concert(user.input);
+            break;
+        default:
+            console.log('please enter a valid command');
+    }
+
+});
 
 // movies:
 
-function movie() {
+function movie(input) {
+
+    // the global input variable is assigned to the local movie variable (for clarity within the function):
 
     let movie = input;
+
+    // checks for a valid movie input:
 
     if (movie === '') {
         console.log("If you haven't watched Mr. Nobody, you should!")
         movie = 'Mr. Nobody'
     }
 
+    // creates a query url for use in the api call to OMDB:
+
     let queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    // sends a request to OMDB using axios:
 
     axios.get(queryURL)
 
         .then(
+
+            // takes the response and logs the relevant data to the console:
 
             function (response) {
                 console.log('------------------------------');
@@ -65,14 +99,20 @@ function movie() {
 
 // music:
 
-function song() {
+function song(input) {
+
+    // global input variable assigned to local variable song (for clarity within the function):
 
     let song = input;
+
+    // checks for a valid song input:
 
     if (song === '') {
         console.log('Listen to this');
         song = 'Mr. Brightside';
     }
+
+    // uses the spotify object (created from the keys object in keys.js) to .search() for a song:
 
     spotify.search({
         type: 'track',
@@ -81,9 +121,12 @@ function song() {
         if (error) {
             return console.log('Error occurred: ' + error);
         }
+
+        // logs the relevant data from the response object:
+
         console.log('------------------------------');
         console.log('Track: ' + data.tracks.items[0].name);
-        console.log('Artist: ' + data.tracks.items[0].artists.name);
+        console.log('Artist: ' + data.tracks.items[0].artists[0].name);
         console.log('Album: ' + data.tracks.items[0].album.name);
         console.log('Spotify Link: ' + data.tracks.items[0].album.external_urls.spotify);
         console.log('------------------------------');
@@ -94,20 +137,52 @@ function song() {
 
 // concerts:
 
-function concert() {
+function concert(input) {
+
+    // global input variable assigned to local artist variable (for clarity within the function):
 
     let artist = input;
+
+    // checks for a valid artist input:
 
     if (movie === '') {
         console.log('Please enter an artist')
     } else {
 
+        // creates a query url using the artist input by the user:
+
         let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+        // uses axios to call the bandsintown api with the artist:
 
         axios.get(queryURL).then(
             function (response) {
-                console.log(response);
+
+                // loops through the events, limiting the number to 10 and logging the relevant information to the console:
+
+                for (let i = 0; i < 9; i++) {
+                    console.log('------------------------------');
+                    // first letter uppercase taken from https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
+                    console.log('Artist: ' + artist.charAt(0).toUpperCase() + artist.slice(1));
+                    console.log('Venue: ' + response.data[i].venue.name);
+
+                    // TO DO: display the datetime formatted with moment.js
+
+                    // let date = response.data[i].datetime.moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+                    // console.log(date);
+                    // console.log('Event date: ' + response.data[i].datetime);
+
+
+                    console.log('------------------------------');
+                }
+
             }
         )
     }
 }
+
+// TO DO:
+
+// moment.js the datetime
+// do-what-it-says
+// output data to log.txt file
